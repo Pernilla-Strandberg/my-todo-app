3e7a6c6dd4b761de44dcec0d13c67efecd3bd9f1
Pernilla Strandberg <pernillastrandberg87@gmail.com>
2023-09-29 08:56:52 +0000

    Set up basic structure of functions and start menu

--- run.py
+++ run.py
@@ -5,15 +5,58 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('my_todo_app')
 
-tasks = SHEET.worksheet('tasks')
 
+tasks = SHEET.worksheet('tasks')
 data = tasks.get_all_values()
 
-print(data)
+
+def add_todo():
+    
+
+def list_todos():
+    
+
+def date_validation():
+    
+
+def mark_done():
+    
+
+
+def main():
+    
+
+    while True:
+        print("\n-------------------\nMy Todo Menu:\n-------------------\n")
+        print("1. Add ToDo")
+        print("2. List ToDos")
+        print("3. Mark ToDo as Done")
+        print("4. Quit App")
+
+        choice = input("\nEnter a choice (1-4) from the menu: ")
+
+        if choice == "1":
+            print("\n-------------------\n1. Add Todo:\n-------------------\n")
+        
+
+        elif choice == "2":
+            print("\n-------------------\n2. List ToDos:\n-------------------\n")
+            
+
+        elif choice == "3":
+            print("\n-------------------\n3. Mark ToDo as Done:\n-------------------\n")
+            
+
+        elif choice == "4":
+            print("\n-------------------\nGood bye!\n-------------------\n")
+            break
+
+if __name__ == "__main__":
+    main()
